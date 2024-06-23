34a037de9ea3eed93c9028df12179a2bd56f0481
zakumann <samuel742able@gmail.com>
2024-06-23 23:50:31 +0900

    Create repository and Player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public CharacterController charCon;
+
+    private Vector3 moveInput;
+
+    public Transform camTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        charCon.Move(moveInput);
+
+        // control camera rotation
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+    }
+}
