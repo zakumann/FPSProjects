44f6a7e457629109c15db39eb274490d712ff00f
zakumann <samuel742able@gmail.com>
2024-06-26 00:07:40 +0900

    Add Jump function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed, gravityModifier, jumpPower;
     public CharacterController charCon;
 
     private Vector3 moveInput;
@@ -17,6 +17,10 @@ public class PlayerController : MonoBehaviour
     public float mouseSensitivity;
     public bool invertX;
     public bool invertY;
+
+    private bool canJump, canDoubleJump;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
 
     // Start is called before the first frame update
     void Start()
@@ -30,15 +34,51 @@ public class PlayerController : MonoBehaviour
         //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
+        //store y velocity
+        float yStore = moveInput.y;
+
         float vertMove = Input.GetAxis("Vertical");
         float horiMove = Input.GetAxis("Horizontal");
 
-        moveInput.Normalize();
+        Vector3 vert = transform.forward * vertMove;
+        Vector3 hori = transform.right * horiMove;
 
-        //Creating the moving vector
-        Vector3 move = transform.right * horiMove + transform.forward* vertMove;
+        moveInput = vert + hori;
+        moveInput = moveInput * moveSpeed;
 
-        charCon.Move(move * moveSpeed * Time.deltaTime);
+        moveInput.y = yStore;
+
+        moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
+
+        if(charCon.isGrounded)
+        {
+            moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, .25f, whatIsGround).Length > 0;
+
+        /*if(canJump)
+        {
+            canDoubleJump = false;
+        }*/
+
+        //Handle jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(canJump)
+            {
+                moveInput.y = jumpPower;
+                canDoubleJump = true;
+            }
+
+            else if (canDoubleJump == true)
+            {
+                moveInput.y = jumpPower;
+                canDoubleJump = false;
+            }
+        }
+
+        charCon.Move(moveInput * moveSpeed * Time.deltaTime);
 
         // control camera rotation
         Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
